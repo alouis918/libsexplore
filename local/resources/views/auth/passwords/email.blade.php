785fa41af5c02ca614aa9d672de48f6b080a631b
DesmondNoutcha <alouis@gmail.com>
2016-09-10 10:45:47 +0200

    Add title

--- local/resources/views/auth/passwords/email.blade.php
+++ local/resources/views/auth/passwords/email.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
-
+@section('title')
+    Email
+@endsection
 <!-- Main Content -->
 @section('content')
 <div class="container">
--- local/resources/views/layouts/app.blade.php
+++ local/resources/views/layouts/app.blade.php
@@ -8,24 +8,64 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Laravel</title>
+    <title>@yield('title')</title>
 
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
-
-    <!-- Styles -->
-
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-
     <style>
-        body {
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
             font-family: 'Lato';
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
         }
+
         .fa-btn {
             margin-right: 6px;
         }
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
     </style>
-    <!-- Styles -->
    <link href="{{URL::to('css/app.css')}}" rel="stylesheet">
    <link href="{{URL::to('DataTables/datatables.min.css')}}" rel="stylesheet">
    <link href="{{URL::to('font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
@@ -47,7 +87,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    Monbord
                 </a>
             </div>
 
